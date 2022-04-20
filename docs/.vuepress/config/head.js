ccae83397535fe81b394973663dfec240bfe9e05
XingtianLiu <1104073376@qq.com>
2022-04-20 14:41:55 +0800

    fix: 修复 gitee 升级导致图片不显示的问题

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -10,6 +10,7 @@ module.exports = [
   ],
   ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+  ['meta', { name: 'referrer', content: 'no-referrer' }]
   [
     'script',
     {
